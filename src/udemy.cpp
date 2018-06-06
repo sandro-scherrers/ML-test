935f543e5d56e15002165d17136b01a7f43931db
srs17 <sandro.scherrers17@imperial.ac.uk>
2018-06-06 11:49:14 +0200

    2

--- src/udemy.cpp
+++ src/udemy.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "Git works!!!" << endl; // prints !!!Hello World!!!
+	cout << "Git works finally!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
